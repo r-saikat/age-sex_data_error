140ac001065244b2c9aaaf2cdf20f56b07a47f7e
r-Saikat <roy.saikatroy99@gmail.com>
2020-12-24 23:09:29 +0530

    Added whipple's indices
    1) Calculated and added Whipple's indices.
    
    2) Corrected the xlab and ylab of the first line diagram. They had been interchanged wrongly at the initial commit.

--- rcode.R
+++ rcode.R
@@ -12,6 +12,9 @@ library(ggplot2)
 library(reshape2)
 mdf <- melt(wbagesexdata, id.vars="age")
 
+
+### This is for plotting the multiple line diagram
+
 linefig <- ggplot(mdf, aes( x=age, y=value, colour=variable, group=variable )) + 
   geom_line() +
   geom_point() +
@@ -19,16 +22,33 @@ linefig <- ggplot(mdf, aes( x=age, y=value, colour=variable, group=variable )) +
   scale_linetype_manual(values=c("total"="dashed", "male"="solid","female"="solid")) +
   scale_x_discrete(breaks = wbagesexdata$age[c(T,F,F,F,F)]) +
   ggtitle("Age-Sex Population Distribution, \n West Bengal (2011)") +
-  xlab("Population") + ylab("Age (lbd)") +
+  ylab("Population") + xlab("Age (lbd)") +
   theme(plot.title = element_text(hjust = 0.5))
 
 linefig
 
 
-
+# This plots the population pyramid
 
 malefemale <- wbagesexdata[,c(3,4,1)]
 library(pyramid)
 
 pyramid(malefemale,Llab="Males", Rlab="Females", Clab="Age (lbd)",main = "Single-year Age Population Pyramid \n West Bengal, 2011", Cstep = 5)
 
+# This calculates the Whipple's indices
+
+attach(wbagesexdata)
+
+age5 <- seq(25,60,5) +1
+totpop5 <- sum(total[age=age5])
+
+age0 <- seq(30,60,10) + 1
+totpop0 <- sum(total[age=age0])
+
+age2362 <- c(23:62) + 1
+totpop2362  <- sum(total[age=age2362])
+
+WI0 <- totpop0 / (totpop2362/10) * 100
+
+WI5 <- totpop5 / (totpop2362/5) * 100
+detach(wbagesexdata)
